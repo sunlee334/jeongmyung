f4a9ecd3b121d8c2455e6301321e8534efdabf9a
Sun Lee <sunlee334@gmail.com>
2025-03-20 19:22:05 +0900

    app bar size config

--- app.vue
+++ app.vue
@@ -8,10 +8,10 @@
         <!-- <v-app-bar-title>정명</v-app-bar-title> -->
 
         <template v-slot:append>
-          <v-btn to="/" variant="text" class="mx-sm-1"><b>홈</b></v-btn>
-          <v-btn to="/equipment" variant="text" class="mx-sm-1"><b>분석장비 및 측정항목</b></v-btn>
-          <v-btn to="/info" variant="text" class="mx-sm-1"><b>측정안내</b></v-btn>
-          <v-btn href="https://docs.google.com/forms/d/1YDLoqGXba3wm3-B1qWLsL3YeLUJxKdVFC_k_m9yC65w" target="_blank" variant="flat" class="mx-sm-1"rounded color="green-darken-4" size="large">
+          <v-btn to="/" variant="text" class="mx-2" size=""><b>홈</b></v-btn>
+          <v-btn to="/equipment" variant="text" class="mx-2" size=""><b>측정항목</b></v-btn>
+          <v-btn to="/info" variant="text" class="mx-2" size=""><b>측정안내</b></v-btn>
+          <v-btn href="https://docs.google.com/forms/d/1YDLoqGXba3wm3-B1qWLsL3YeLUJxKdVFC_k_m9yC65w" target="_blank" variant="flat" rounded color="green-darken-4" size="large">
             문의하기
           </v-btn>
         </template>
@@ -26,3 +26,18 @@
 
   </NuxtLayout>
 </template>
+<script>
+  export default {
+    computed: {
+      btnSize () {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 220
+          case 'sm': return 400
+          case 'md': return 500
+          case 'lg': return 600
+          case 'xl': return 800
+        }
+      },
+    },
+  }
+</script>
